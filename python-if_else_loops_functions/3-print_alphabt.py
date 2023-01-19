56c90245a0c5544af5e5444883cf178b3001f1e2
Maurlcio <thecoolermess@gmail.com>
2023-01-19 08:20:20 -0800

    fun one

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 for letter in range(ord("a"), (ord("z") + 1)):
-    if letter in [101,113]:
+    if letter in [101, 113]:
         continue
     txt = "{letter}"
     print(txt.format(letter=chr(letter)), end='')
