2b835b40c82fd0882e554e6173882b17dccf384c
Maurlcio <thecoolermess@gmail.com>
2023-01-20 12:47:44 -0800

    thank GOD i had my code for this exercise in C lying around. i did NOT want to figure out the math for this one again, and i got to focus on learning python syntax

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 != 0:
+            print(f" Fizz", end='')
+        elif i % 5 == 0 and i % 3 != 0:
+            print(f" Buzz", end='')
+        elif i % 3 == 0 and i % 5 == 0:
+            print(f" FizzBuzz", end='')
+        elif i == 1:
+            print(f"{i}", end='')
+        else:
+            print(f" {i}", end='')
+    print("")
