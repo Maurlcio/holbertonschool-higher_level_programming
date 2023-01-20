0644d9a6916e5068c53643084e3d7f085c3c998c
Maurlcio <thecoolermess@gmail.com>
2023-01-20 10:53:59 -0800

    i spent so long trying to figure out how to make the newline work properly and it turns out all i had to figure out was to just put an empty print at the end of the function. if this doesnt pass the checker im gonna commit atrocities

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(string):
+    for i in string:
+        if ord(i) in range(97, 123):
+            print("{}".format(chr(ord(i) - 32)), end='')
+        else:
+            print("{}".format(i), end='')
+    print("")
