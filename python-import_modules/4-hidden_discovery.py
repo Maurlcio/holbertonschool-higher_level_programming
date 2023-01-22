f1f71c3b8130134679b8c4acde1cf2b492f843ad
Maurlcio <thecoolermess@gmail.com>
2023-01-22 11:03:52 -0800

    unsure if this works, testing on checker

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+
+    def whomst():
+        for i in dir(hidden_4):
+            if i[:2] == "__":
+                pass
+            else:
+                print(x)
+    whomst()
