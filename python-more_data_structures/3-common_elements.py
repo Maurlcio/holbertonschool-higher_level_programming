25ddf5620746404d198d91c635e1892937a8b46b
Maurlcio <thecoolermess@gmail.com>
2023-01-27 13:34:13 -0800

    only common elements/only different elements

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -5,4 +5,3 @@ def common_elements(set_1, set_2):
         if i in set_2:
             new_list.append(i)
     return new_list
-
--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2):
+def only_diff_elements(set_1, set_2):
     new_list = []
     for i in set_1:
         if i not in set_2:
             new_list.append(i)
-    for l in set_2:
-        if l not in set_1:
-            new_list.append(l)
+    for x in set_2:
+        if x not in set_1:
+            new_list.append(x)
     return new_list
-
