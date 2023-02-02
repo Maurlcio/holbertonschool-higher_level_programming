7f7b2f3cd788a668488d1d2168002bd6c24ef841
Maurlcio <thecoolermess@gmail.com>
2023-02-02 05:08:18 -0800

    defines a private attribute

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" square class that works off of exercise 0
+"""
+
+
+class Square:
+    """sets up a private attribute called size
+    """
+    def __init__(self, size):
+        self.__size = size
