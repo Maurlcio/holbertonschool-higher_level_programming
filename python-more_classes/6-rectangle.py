a1a15de4059d51358ba385061cb1891b0f5c157a
Maurlcio <thecoolermess@gmail.com>
2023-02-05 11:29:30 -0800

    added variable that keeps track of instance addings and deletions

---
+++ python-more_classes/6-rectangle.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""rectangle at a rect angle"""
+
+
+class Rectangle:
+    """hold on im countin ere"""
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        type(self).number_of_instances += 1
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    def area(self):
+        return (self.width * self.height)
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            return ((self.width + self.height) * 2)
+
+    def __str__(self):
+        if self.width == 0 or self.height == 0:
+            return ("")
+        rectanglePrint = []
+        for column in range(self.height):
+            for row in range(self.width):
+                rectanglePrint.append("#")
+            if column != self.height - 1:
+                rectanglePrint.append("\n")
+        return ("".join(rectanglePrint))
+
+    def __repr__(self):
+        return(f"Rectangle({self.width}, {self.height})")
+
+    def __del__(self):
+        type(self).number_of_instances -= 1
+        print("Bye rectangle...")
