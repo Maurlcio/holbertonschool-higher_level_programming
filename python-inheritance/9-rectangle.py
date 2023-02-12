f651599e1783bf74ee1a1f565097bde3d142fc42
Maurlcio <thecoolermess@gmail.com>
2023-02-12 11:20:30 -0800

    super rectangle

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """the shebang"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""yup, there it is, its see who it is"""
 
 
 class Rectangle(BaseGeometry):
@@ -13,9 +14,11 @@ class Rectangle(BaseGeometry):
         self.__height = height
 
     def area(self):
+        """its look who it is"""
         return self.__width * self.__height
 
     def __str__(self):
+        """as in, look who it is"""
         string = "[" + str(self.__class__.__name__) + "] "
         string += str(self.__width) + "/" + str(self.__height)
         return string
