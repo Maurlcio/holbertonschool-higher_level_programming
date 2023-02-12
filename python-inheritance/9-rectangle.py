727a4e16b8e7ab3066e714e2af733f014858466e
Maurlcio <thecoolermess@gmail.com>
2023-02-12 11:24:38 -0800

    rectangle

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-"""the shebang"""
+"""Represents a rectangle using a prior thing"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
-"""yup, there it is, its see who it is"""
 
 
 class Rectangle(BaseGeometry):
-    """inheritance class"""
+    """Represents a rectangle using BaseGeometry"""
+
     def __init__(self, width, height):
-        """yup, there it is"""
+        """Initialize a new Rectangle"""
         super().integer_validator("width", width)
         self.__width = width
         super()integer_validator("height", height)
         self.__height = height
 
     def area(self):
-        """its look who it is"""
+        """Returns the area"""
         return self.__width * self.__height
 
     def __str__(self):
-        """as in, look who it is"""
+        """Returns print() and str() correctly"""
         string = "[" + str(self.__class__.__name__) + "] "
         string += str(self.__width) + "/" + str(self.__height)
         return string
