156ad053e08356a73ab6b8389ac93cdeb0ecbfb3
Maurlcio <thecoolermess@gmail.com>
2023-02-12 11:16:30 -0800

    super geometry

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -4,8 +4,9 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """inheritance"""
+    """inheritance class"""
     def __init__(self, width, height):
+        """yup, there it is"""
         super().integer_validator("width", width)
         self.__width = width
         super()integer_validator("height", height)
