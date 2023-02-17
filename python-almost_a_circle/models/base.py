4ad05c08253ce4b4916bef03e8ad410d7b99d325
Maurlcio <thecoolermess@gmail.com>
2023-02-17 09:54:53 -0800

    base class and package creation

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""the shebang"""
+
+
+class Base:
+    """class that will serve as a base for all the other ones"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
