1992588b9f41c7aec46569bdc248dba90e41900b
Maurlcio <thecoolermess@gmail.com>
2023-04-16 09:48:54 -0700

    fixed exercise 4 because i coded it a little bit too well lol

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -2,10 +2,4 @@
 
 const TWO_ARGS = [process.argv[2], process.argv[3]];
 
-if (TWO_ARGS[0] === undefined) {
-  console.log('No arguments found');
-} else if (TWO_ARGS[1] === undefined) {
-  console.log('You need 2 arguments for this bozo');
-} else {
-  console.log(TWO_ARGS[0] + ' is ' + TWO_ARGS[1]);
-}
+console.log(TWO_ARGS[0] + ' is ' + TWO_ARGS[1]);
