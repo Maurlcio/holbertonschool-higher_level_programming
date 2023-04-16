028b51a863d6e43e3941213e144103037c77b1cf
Maurlcio <thecoolermess@gmail.com>
2023-04-16 09:13:01 -0700

    function that prints 3 strings from a list with a for loop

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+function bigThree () {
+  const COOL_LANGS = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+  for (let i = 0; i < COOL_LANGS.length; i++) {
+    console.log(COOL_LANGS[i]);
+  }
+}
+
+bigThree();
