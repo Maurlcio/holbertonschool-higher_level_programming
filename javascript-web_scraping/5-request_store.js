c9fa06eaaf5c64a7bcb33ba18b302cfaa0147a27
Maurlcio <thecoolermess@gmail.com>
2023-04-18 07:24:29 -0700

    script that gets the contents of a specific webpage and stores its contents in a file

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -4,6 +4,4 @@ const request = require('request');
 
 const URL = request(process.argv[2]);
 
-fs.writeFile(process.argv[3], URL, (err) => {
-  if (err) throw (err);
-});
+fs.createWriteStream(process.argv[3], URL);
