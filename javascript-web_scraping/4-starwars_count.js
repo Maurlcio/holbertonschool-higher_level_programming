5a17458173606fdcb42dab63c5c535ef2509cfda
Maurlcio <thecoolermess@gmail.com>
2023-04-18 07:10:22 -0700

    returns all the starwars movies with a specific character in them

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -2,13 +2,13 @@
 
 const request = require('request');
 
-request(process.argv[2], function(error, response, body) {
+request (process.argv[2], function(error, response, body) {
   if (!error) {
     const RESULTS = JSON.parse(body).results;
     console.log(RESULTS.reduce((count, movie) => {
       return movie.characters.find((character) => character.endsWith('/18/'))
-	? count + 1
-	: count ;
+        ? count + 1
+        : count;
     }, 0));
   }
 });
