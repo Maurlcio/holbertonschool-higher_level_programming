39a152178a60c1c2b8f8d03636970adbf6d36d44
Maurlcio <thecoolermess@gmail.com>
2023-04-18 06:42:28 -0700

    script that prints the title of a star wars movie with the given number, assuming the number matches a pre-existing mainline film

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -3,7 +3,7 @@
 const request = require('request');
 
 const URL = 'https://swapi-api.hbtn.io/api/films/:' + process.argv[2];
-request(url, function (error, response, body) {
+request(URL, function (error, response, body) {
   console.error('error:', error);
   console.log(JSON.parse(body).title);
 });
