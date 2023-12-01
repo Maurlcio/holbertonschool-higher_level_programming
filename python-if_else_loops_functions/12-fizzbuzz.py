aa1837aa6857c3e43fddad5b708b680971eecd21
Maurlcio <thecoolermess@gmail.com>
2023-12-01 18:05:04 -0300

    my original attempt at this was embarassing, so I redid it to be more elegant.

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 101):
-        if i % 3 == 0 and i % 5 != 0:
-            print(f" Fizz", end='')
-        elif i % 5 == 0 and i % 3 != 0:
-            print(f" Buzz", end='')
-        elif i % 3 == 0 and i % 5 == 0:
-            print(f" FizzBuzz", end='')
-        elif i == 1:
-            print(f"{i}", end='')
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif i % 3 == 0:
+            print("Fizz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
         else:
-            print(f" {i}", end='')
-    print(" ", end='')
+            print(f"{i}", end=" ")
